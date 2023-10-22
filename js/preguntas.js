f88e5c6bb89331b83c40cf539d63d703a843ad39
Tere Fernández <132773981+TereEstefania@users.noreply.github.com>
2023-10-21 21:40:20 -0300

    Update preguntas.js

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,24 +1,14 @@
-function myFunction1() {
-    document.getElementById("uno").style.display = 'block';
-  }
+/**
+ * funcion del el evento que realiza el boton
+ * resive como parametro el id del <div></div> corrspondiente
+ * evalua el estado de su elemento
+ */
+function myFunction(idTexto){
+const texto = document.getElementById(idTexto);
 
-
-  function myFunction2() {
-    document.getElementById("dos").style.display = 'block';
-  }
-
-  function myFunction3() {
-    document.getElementById("tres").style.display = 'block';
-  }
-    
-  function myFunction4() {
-    document.getElementById("cuatro").style.display = 'block';
-  }
-
-  function myFunction5() {
-    document.getElementById("cinco").style.display = 'block';
-  }
-
-  function myFunction6() {
-    document.getElementById("seis").style.display = 'block';
-  }
+  if (texto.style.display === 'none') {
+    texto.style.display = 'block'; // Mostrar el elemento
+  } else {
+    texto.style.display = 'none'; // Ocultar el elemento
+  }   
+} 
